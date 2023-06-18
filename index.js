0a5620bad6339589e6a32bcf018370668826449a
wjt0078 <133739371+wjt0078@users.noreply.github.com>
2023-06-19 04:16:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
 
 const configuration = new Configuration({
-  apiKey: '##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##',
+  apiKey: '##you ki##',
 });
 
 const openai = new OpenAIApi(configuration);
